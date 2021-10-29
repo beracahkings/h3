edfe4a914ce027003a1e678603e6dd357cd41987
beracah-kings <kings360@outlook.in>
2021-10-29 19:52:05 +0530

    App2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <Test></Test>
+     <Test company="Beracah Consulting"></Test>
      
       </header>
     </div>
