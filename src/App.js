4b2767f6e54df6c53f1d62595cdf0cf60cf184a0
beracah-kings <kings360@outlook.in>
2021-10-30 17:00:09 +0530

    newupdate

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <Test company="Beracah Consulting Services"></Test>
+     <Test company="Beracah Consulting Services">Chennai</Test>
      
       </header>
     </div>
--- src/Test.js
+++ src/Test.js
@@ -3,7 +3,8 @@ function Test(props)
 	return(
 		<div>
 		<h1>Kingston Abraham</h1><br/>
-		<h3>{props.company}</h3>
+		<h3>{props.company}</h3><br/>
+		<h3>{props.children}</h3>
 		</div>
 		);
 }
